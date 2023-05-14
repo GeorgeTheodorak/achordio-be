39bba0e1e0742b4d50667edbed3a140ae32edbd9
GeorgeTheodorak <86367329+GeorgTheodorak@users.noreply.github.com>
2023-05-14 22:39:38 +0300

    freeze bat for easier pip freeze . requirements update . main fix for crash.

--- main.py
+++ main.py
@@ -1,30 +1,18 @@
+import os
 from uuid import uuid4
+from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 from starlette import status
-
 from auth.hash import get_hashed_password
 
 app = FastAPI()
 
+load_dotenv(".env")  # Load environment variables from .env file
 
 @app.get("/greet")
 async def root():
-    return {"message": "All good"}
+    return {"message": "All good","envFileTest":os.environ.get("postGresPassword")}
 
 
-@app.post('/signup', summary="Create new user", response_model=UserOut)
-async def create_user(data: UserAuth):
-    # querying database to check if user already exist
-    user = db.get(data.email, None)
-    if user is not None:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="User with this email already exist"
-        )
-    user = {
-        'email': data.email,
-        'password': get_hashed_password(data.password),
-        'id': str(uuid4())
-    }
-    db[data.email] = user  # saving user to database
-    return user
+
+
