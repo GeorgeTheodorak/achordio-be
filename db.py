dea1c758f99b60520f1874de4d2f24663f159aa0
GeorgeTheodorak <86367329+GeorgTheodorak@users.noreply.github.com>
2023-05-22 20:09:53 +0300

    Update.

--- db.py
+++ db.py
@@ -7,8 +7,10 @@ def check_postgres_ready(connection_string):
     while True:
         try:
             print("Trying to connect to db.")
+            time.sleep(3)
             engine = create_engine(connection_string)
             with engine.connect():
+                print("CONNEXCTED TO DB")
                 return engine
         except OperationalError:
             print("no active db was found. retrying.")
