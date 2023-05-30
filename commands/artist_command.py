5253ebff92ea45dccca84e8e03c82df6955695ca
elevchyt <lehy64@gmail.com>
2023-05-30 23:08:00 +0300

    create artist command fix

--- commands/artist_command.py
+++ commands/artist_command.py
@@ -6,6 +6,7 @@ from last_fm.last_fm_sdk import LastFmSdk
 
 
 def createBaseTopArtists(genre, url: str, api_key: str):
+    print("starting")
     db = SessionLocal()
     last_fm_sdk = LastFmSdk(url, api_key)
 
@@ -13,21 +14,24 @@ def createBaseTopArtists(genre, url: str, api_key: str):
 
     for artist in top_artists['topartists']['artist']:
 
-        try:
-            mb_id = artist["mbid"]
-        except Exception as e:
+        artist_name = artist['name']
+        if "mbid" in artist:
+            # Access the value associated with the 'mbid' key
+            mbid = artist["mbid"]
+        else:
             continue
 
-        artist_new = db.query(Artists).filter(Artists.music_brainz_identifier == mb_id).first()
+        artist_new = db.query(Artists).filter(Artists.music_brainz_identifier == mbid).first()
 
-        if artist_new is None:
+        if artist_new is not None:
             continue
+
 
         artist_new = Artists()
         artist_new.name = artist['name']
-        artist_new.music_brainz_identifier = mb_id
+        artist_new.music_brainz_identifier = mbid
         artist_new.description = ""
 
         db.add(artist_new)
         db.commit()
-        print(f"Addded new artist with name {artist_new.name} and id {artist_new.id}")
+        print(f"Added new artist with name {artist_new.name} and id {artist_new.id}")
